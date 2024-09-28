87a5725fdd8ce3d1c7d155241076f401b85f29a5
igormat7096 <igornovais.tj96@gmail.com>
2024-09-27 21:35:46 -0300

    lista das 15 cidades

---
+++ app.py
@@ -0,0 +1,11 @@
+cidades =["São Paulo","Rio de Janeiro","Belo Horizonte","Brasília","Salvador","Fortaleza","Curitiba",
+"Recife","Porto Alegre","Manaus","Belém","Goiânia","Natal","São Luís","João Pessoa"]
+
+cidades.sort()
+print("Lista em ordem alfabética:\n")
+for i in range(len(cidades)): 
+ print(f"{i+1}°nome da lista: {cidades[i]}.")
+    
+    
+
+
